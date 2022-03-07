3a823b71d6f266efdff4638c39c0bb24cfb04c16
cchosch <colinhoscheit@gmail.com>
2022-03-07 15:03:50 +0000

    adding comments

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # rust-cli-tic-tac-toe
-This is part of my journey learning rust. This is 
-advanced as it is something that I have never written 
-in any other language before.
+I am learning rust and 
--- src/main.rs
+++ src/main.rs
@@ -166,17 +166,20 @@ impl TTTBoard{
 
 fn main() {
     println!("Tic tac toe in Rust vs UNBEATABLE AI");
+    // initialize vars
     let mut line;
     let mut board = TTTBoard{.. Default::default()};
     let mut x; 
     let mut y;
     let mut win_yet;
 
-    println!("| (0, 0) | (0, 1) | (0, 2) |");
-    println!("----------------------------");
-    println!("| (1, 0) | (1, 1) | (1, 2) |");
-    println!("----------------------------");
-    println!("| (2, 0) | (2, 1) | (2, 2) |");
+    // display x, y format
+    println!("| 0, 0 | 0, 1 | 0, 2 |");
+    println!("----------------------");
+    println!("| 1, 0 | 1, 1 | 1, 2 |");
+    println!("----------------------");
+    println!("| 2, 0 | 2, 1 | 2, 2 |");
+    // gameloop
     loop {
         println!("Make a move x,y");
         
@@ -185,11 +188,19 @@ fn main() {
             Err(e) =>{ println!("ERROR {}", e);}
         }
         line = String::new();
+        // get user input
         stdin().read_line(&mut line).unwrap();
+        // cut of \n \r or any other stuff like that
         line = String::from(line.trim());
+        if line == "q"{ // if user enters q, quit the game
+            break;
+        }
+        // split the input between ,
         let newpos : Vec<&str> = line.split(",").collect();
+        // if there are more than 2 sections after we split from ,
         if newpos.len() != 2{ continue; }
-        
+
+        // get integer value from user input
         match newpos[0].trim().parse::<usize>(){
             Ok(o) => x = o,
             Err(_) => continue,
@@ -198,36 +209,41 @@ fn main() {
             Ok(o) => y = o,
             Err(_) => continue,
         }
-        
+
+        // make given move as the player, (O)
         board.make_move((x, y), BoardState::O);
+        // check if there is a winner
         win_yet = board.winner();
-        match *win_yet {
-            BoardState::Empty => {
-                if board.full() {
+        match win_yet {
+            BoardState::Empty => { // if board.winner() return Empty
+                if board.full() { // if the board is full
                     println!("TIE");
+                    // break the gameloop
                     break;
                 }
             },
-            BoardState::O | BoardState::X => {
+            _ => { // any other state
                 if *win_yet == BoardState::O{
                     println!("{}YOU WON\n", board);
                 }else {
                     println!("{}You Lost\n", board);
                 }
+                // break the gameloop
                 break;
             },
-            
         }
+        // make computer move
         minimax(&mut board);
+        // check winner again
         win_yet = board.winner();
-        match *win_yet {
+        match win_yet {
             BoardState::Empty => {
                 if board.full() {
                     println!("TIE");
                     break;
                 }
             },
-            BoardState::O | BoardState::X => {
+            _ => {
                 if *win_yet == BoardState::O{
                     println!("{}YOU WON\n", board);
                 }else {
@@ -238,38 +254,31 @@ fn main() {
             
         }
         println!("{}", board);
-        if line == "q"{
-            break;
-        }
     }
 
 }
 
 
 fn minimax(board : &mut TTTBoard){
-    let mut boardc = board.clone();
     let mut res;
     let mut best = (0, (0, 0));
-    let mut d = 0;
-    for r in 0..boardc.board.len(){
+    for r in 0..board.board.len(){
         for s in 0..board.board[r].len(){
             if board.board[r][s] != BoardState::Empty{
                 continue
             }
-            boardc.make_move((r, s), BoardState::X);
-            res = eval(&mut boardc, BoardState::O, false);
+            board.make_move((r, s), BoardState::X);
+            res = eval(board, BoardState::O, false);
             if res > best.0{
                 best = (res, (r, s));
                 if res == 2{
-                    board.copy(&boardc);
                     return;
                 }
             }
-            boardc.make_move((r, s), BoardState::Empty);
+            board.make_move((r, s), BoardState::Empty);
         }
     }
-    boardc.make_move((best.1.0, best.1.1), BoardState::X);
-    board.copy(&boardc);
+    board.make_move((best.1.0, best.1.1), BoardState::X);
     return
 }
 
@@ -280,11 +289,12 @@ fn eval(board : &mut TTTBoard, cplayer : BoardState, max :bool) -> i8{
      * tie = 1
      * win = 2
      */
-    
+    // check winner
     match board.winner(){
         BoardState::Empty => {if board.full(){ return 1; }},
         s => {
             if max { 
+                // if we 
                 if *s == cplayer{
                     return 2;
                 }
@@ -297,29 +307,34 @@ fn eval(board : &mut TTTBoard, cplayer : BoardState, max :bool) -> i8{
         },
     }
     //          (best, (x, y))
-    let mut optimal: i8 = 0;
-    if !max{
+    let mut optimal: i8 = 0; // initialize optimal variable
+    if !max{ // if we are minimizing set the optimal to 2
         optimal = 2;
     }
     let mut result: i8;
+    // loop through board
     for row in 0..board.board.len(){
         for column in 0..board.board[0].len(){
+            // if we can make a move on (row, column)
             if board.board[row][column] == BoardState::Empty{
+                // make the move that we are going to evaluate
                 board.make_move((row, column), cplayer.clone());
+                // evaluate
                 result = eval(board, cplayer.opposite(), !max);
+                // reverse move
                 board.make_move((row, column), BoardState::Empty);
-                if max{ 
-                    if result > optimal{
+                if max{ // if we are maximizing player
+                    if result > optimal{ // maximize optimal
                         optimal = result;
                     }
-                }else{ 
-                    if result < optimal{
+                }else{ // if we are minimizing player
+                    if result < optimal{ // minimize optimal
                         optimal = result;
                     }
                 }
             }
         }
     }
-    return optimal
+    return optimal // return 
 }
 
